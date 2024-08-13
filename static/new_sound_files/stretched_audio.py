b3d7d495e207b9f669ead549f91ade856ca02668
jaeyoung-leee <ljy0111@gachon.ac.kr>
2024-08-13 11:33:25 +0900

    C4 format

--- static/new_sound_files/stretched_audio.py
+++ static/new_sound_files/stretched_audio.py
@@ -4,18 +4,19 @@ import numpy as np
 import os
 
 # 대상 파일 리스트
-input_dir = '/Users/two_jyy/project_openSW/sound_files_drum'
-output_dir = '/Users/two_jyy/project_openSW/new_sound_files'
+input_dir = '/Users/two_jyy/project_openSW/thinkthing/sound_files'
+output_dir = '/Users/two_jyy/project_openSW/thinkthing/static/new_sound_files'
 start_index = 25
-end_index = 37
+end_index = 36
 
+index = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
 # 타겟 길이 (초 단위)
 target_length = 4.0
 
 # 파일 처리 루프
 for i in range(start_index, end_index + 1):
     filename = f'{input_dir}/{i}.wav'
-    output_filename = f'{output_dir}/stretched_audio_{i}.wav'
+    output_filename = f'{output_dir}/{index[(i-start_index) % len(index)]}4.wav'
     
     # 음원 파일 불러오기
     y, sr = librosa.load(filename, sr=None)
